b9cb3027ce928dc3e114b21492da3fc869fcefec
sobolevn <mail@sobolevn.me>
2020-05-08 18:25:10 +0300

    Regular update

--- vscode/install.sh
+++ vscode/install.sh
@@ -3,10 +3,12 @@ code --install-extension alexkrechik.cucumberautocomplete
 code --install-extension dariofuzinato.vue-peek
 code --install-extension EditorConfig.EditorConfig
 code --install-extension justusadam.language-haskell
+code --install-extension mikestead.dotenv
 code --install-extension mjmcloug.vscode-elixir
 code --install-extension ms-python.python
 code --install-extension ms-vscode.sublime-keybindings
 code --install-extension octref.vetur
+code --install-extension pnp.polacode
 code --install-extension rust-lang.rust
 code --install-extension stevejpurves.cucumber
 code --install-extension teabyii.ayu
