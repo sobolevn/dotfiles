c4e3f30410a4401fb88ac40f1c1c0d8855dd9e20
sobolevn <mail@sobolevn.me>
2021-01-14 19:30:11 +0300

    Fixes codespaces

--- vscode/install.sh
+++ vscode/install.sh
@@ -8,12 +8,13 @@ if [ ! $(command -v code-insiders) ]; then
   ls $HOME/.vscode-remote/bin
   VSCODE="$HOME/.vscode-remote/bin/$(ls "$HOME/.vscode-remote/bin")"
   PATH="$VSCODE:$PATH"
+  echo "Added $VSCODE to the PATH"
 fi
 
 if [ ! $(command -v code) ] && [ $(command -v code-insiders) ]; then
   # Link `code` to be the same as `code-insiders`:
   ln -s "$(which code-insiders)" "$(dirname "$(which code-insiders)")/code"
-  echo 'linked `code-insiders` to be accessible as `code`'
+  echo 'Linked `code-insiders` to be accessible as `code`'
 fi
 
 # Install plugins:
