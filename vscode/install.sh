77456d41da6f813abe73b34df8d04b146ec9ade9
sobolevn <mail@sobolevn.me>
2021-01-14 17:19:57 +0300

    Fixes codespaces

--- vscode/install.sh
+++ vscode/install.sh
@@ -3,6 +3,14 @@
 set -o errexit
 set -o nounset
 
+if [ ! $(command -v code) ]; then
+  echo 'not -v code'
+fi
+
+if [ $(command -v code-insiders) ]; then
+  echo '-v code-insiders'
+fi
+
 if [ ! $(command -v code) ] && [ $(command -v code-insiders) ]; then
   # Link `code` to be the same as `code-insiders`:
   ln -s "$(which code-insiders)" "$(dirname "$(which code-insiders)")/code"
