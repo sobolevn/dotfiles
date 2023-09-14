09c1caf60735d0b1e81facf36f689d3401f6b5dd
sobolevn <mail@sobolevn.me>
2023-09-14 15:27:31 +0300

    Regular update

--- crontab/streamdeck.sh
+++ crontab/streamdeck.sh
@@ -3,12 +3,15 @@
 # Script that automatically opens "StreamDeck.app" when StreamDeck is connected.
 # It also switches it off, when StreamDeck is disconnected from USB drive.
 
-if ioreg -p IOUSB | grep -q "Stream Deck"; then
-  if ! pgrep "Stream Deck"; then
-    open -g -a "Stream Deck.app"
+PROCESS_NAME='Stream Deck'
+APP_NAME="Elgato ${PROCESS_NAME}.app"
+
+if ioreg -p IOUSB | grep -q "$PROCESS_NAME"; then
+  if ! pgrep "$PROCESS_NAME"; then
+    open -g -a "$APP_NAME"
   fi
 else
-  if pgrep -q "Stream Deck"; then
-    killall "Stream Deck"
+  if pgrep -q "$PROCESS_NAME"; then
+    killall "$PROCESS_NAME"
   fi
 fi
